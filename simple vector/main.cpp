dfa0ec5aa0b849c15293ad865a60efaf0a46a256
Antony Orlovsky <orlovskiy@niisi.ru>
2021-11-10 00:10:38 +0300

    Added copy ctor and operator= to simple vector

--- simple vector/main.cpp
+++ simple vector/main.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <numeric>
+#include <tuple>
+
 using namespace std;
 
 void TestConstruction() {
@@ -36,9 +39,23 @@ void TestPushBack() {
     ASSERT(equal(begin(v), end(v), begin(expected)));
 }
 
+void TestCopyAssignment() {
+    SimpleVector<int> numbers(10);
+    iota(numbers.begin(), numbers.end(), 1);
+
+    SimpleVector<int> dest;
+    ASSERT_EQUAL(dest.Size(), 0u);
+
+    dest = numbers;
+    ASSERT_EQUAL(dest.Size(), numbers.Size());
+    ASSERT(dest.Capacity() >= dest.Size());
+    ASSERT(equal(dest.begin(), dest.end(), numbers.begin()));
+}
+
 int main() {
     TestRunner tr;
     RUN_TEST(tr, TestConstruction);
     RUN_TEST(tr, TestPushBack);
+    RUN_TEST(tr, TestCopyAssignment);
     return 0;
 }
--- simple vector/simple_vector.h
+++ simple vector/simple_vector.h
@@ -1,18 +1,26 @@
 #pragma once
 
 #include <cstdlib>
+#include <algorithm>
+
+using namespace std;
 
 template <typename T>
 class SimpleVector {
 public:
     SimpleVector();
     explicit SimpleVector(size_t size);
+    SimpleVector(const SimpleVector& other);
     ~SimpleVector();
 
     T& operator[](size_t index);
+    SimpleVector& operator=(const SimpleVector& other);
 
     T* begin();
     T* end();
+
+    const T* begin() const;
+    const T* end() const;
 
     size_t Size() const;
     size_t Capacity() const;
@@ -31,11 +39,18 @@ SimpleVector<T>::SimpleVector()
     , capacity_(0) {}
 
 template <typename T>
-SimpleVector<T>::SimpleVector(size_t size) {
-    data_ = new T[size];
-    size_ = size;
-    capacity_ = size;
-}
+SimpleVector<T>::SimpleVector(size_t size)
+    : data_(new T[size])
+    , size_(size)
+    , capacity_(size) {}
+
+template<typename T>
+SimpleVector<T>::SimpleVector(const SimpleVector& other)
+    : data_(new T[other.Capacity()])
+    , size_(other.Size())
+    , capacity_(other.Capacity()) {
+        copy(other.begin(), other.end(), begin());
+    }
 
 template <typename T>
 SimpleVector<T>::~SimpleVector() {
@@ -47,6 +62,22 @@ T& SimpleVector<T>::operator[](size_t index) {
     return data_[index];
 }
 
+template <typename T>
+SimpleVector<T>& SimpleVector<T>::operator=(const SimpleVector<T>& other) {
+    if (other.Size() <= capacity_) {
+        copy(other.begin(), other.end(), begin());
+        size_ = other.Size();
+    }
+    else {
+        SimpleVector<T> tmp = other;
+        swap(tmp.data_, data_);
+        swap(tmp.size_, size_);
+        swap(tmp.capacity_, capacity_);
+    }
+
+    return *this;
+}
+
 template<typename T>
 T* SimpleVector<T>::begin() {
     return data_;
@@ -54,6 +85,16 @@ T* SimpleVector<T>::begin() {
 
 template<typename T>
 T* SimpleVector<T>::end() {
+    return data_ + size_;
+}
+
+template<typename T>
+const T* SimpleVector<T>::begin() const {
+    return data_;
+}
+
+template<typename T>
+const T* SimpleVector<T>::end() const {
     return data_ + size_;
 }
 
